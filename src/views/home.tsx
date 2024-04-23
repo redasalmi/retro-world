83822499899a98c3cc841fb593c87f503e309a1a
Reda Salmi <reda.salmi.elt@gmail.com>
2024-04-23 15:53:09 +0100

    adds missing tailwind package

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -2,6 +2,7 @@ export default function Home() {
 	return (
 		<div className="flex h-screen items-center justify-center">
 			<h1 className="text-2xl">Hello React!</h1>
+			<button>TEST</button>
 		</div>
 	);
 }
