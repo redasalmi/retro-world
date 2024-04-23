3e3abc4774a54b4b8cc60d6745f37dee7b87c82c
Reda Salmi <reda.salmi.elt@gmail.com>
2024-04-23 14:25:27 +0100

    updates eslint config

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/home';
 
 ReactDOM.createRoot(document.querySelector('#app')).render(
   <React.StrictMode>
     <MemoryRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="flex h-screen justify-center items-center">
-              <h1 className="text-2xl">Hello React!</h1>
-            </div>
-          }
-        />
+        <Route path="/" element={<Home />} />
       </Routes>
     </MemoryRouter>
   </React.StrictMode>,
---
+++ src/views/home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <h1 className="text-2xl">Hello React!</h1>
+    </div>
+  );
+}
